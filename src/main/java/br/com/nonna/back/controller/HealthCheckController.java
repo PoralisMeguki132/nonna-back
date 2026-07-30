775085ef300ab6aa0368a4af1a4d40e92ff8c02f
PoralisMeguki132 <poralismeguki@gmail.com>
2026-07-29 21:43:20 -0300

    Meu Commitu

---
+++ src/main/java/br/com/nonna/back/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package br.com.nonna.back.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("health-check/liveness")
+    public String liveness(){
+        return "OK";
+    }
+}
